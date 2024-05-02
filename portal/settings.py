ec2e69ac2cee3489bb4b0d9b951563a9a0d8a4bf
PiotrGrabias <komputer2901@wp.pl>
2024-05-02 13:22:29 +0200

    Setup of django backend(users, products, user authentication)

--- portal/settings.py
+++ portal/settings.py
@@ -173,4 +173,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "komputer290123@gmail.com"
-EMAIL_HOST_PASSWORD = 'kaip sprk tuqk jpqw'
+EMAIL_HOST_PASSWORD = ''
